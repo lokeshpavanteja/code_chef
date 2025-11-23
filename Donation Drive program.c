8eed755fb69d37e2d2131b4fd968f78e33dfbba2
lokeshpavanteja <lokeshpavantejakamparapu@gmail.com>
2025-11-23 21:40:02 +0530

    Add files via upload

---
+++ Donation Drive program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int N,X;
+	    scanf("%d%d",&N,&X);
+	    int s=N-X;
+	    printf("%d\n",s);
+	    
+	}
+	return 0;
+
+}
+
