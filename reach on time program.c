9627b8b9de7a1de06f76e79d8745275e140ba461
lokeshpavanteja <lokeshpavantejakamparapu@gmail.com>
2025-11-24 21:09:56 +0530

    Add files via upload

---
+++ reach on time program.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() 
+{
+	int T;
+	scanf("%d",&T);
+	while(T--)
+	{
+	    int X;
+	    scanf("%d",&X);
+	    if(X>=30){
+	        printf("YES\n");
+	        
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	     
+	}
+	return 0;
+
+}
+
