1aa50829fb64bc3cf6bb149bf524c054401e2239
lokeshpavanteja <lokeshpavantejakamparapu@gmail.com>
2025-11-26 21:18:08 +0530

    Add files via upload

---
+++ Who is taller! program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+	int T,X,Y;
+	scanf("%d",&T);
+	while(T--){
+	    scanf("%d%d",&X,&Y);
+	    if(X>Y){
+	        printf("A\n");
+	        
+	    }
+	    else{
+	        printf("B\n");
+	        
+	    }
+	}
+return 0;
+}
+
